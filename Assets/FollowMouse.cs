3f014da375ff3baac34c6cbdbdbdd1ce06c9dcfd
psyGamer <psygamer21@gmail.com>
2022-05-21 20:08:53 +0200

    Initial commit

---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private bool isMouseDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isMouseDown = Input.GetKey(KeyCode.Mouse0);
+
+        transform.position = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) + new Vector3(0,0,10);
+    }
+}
